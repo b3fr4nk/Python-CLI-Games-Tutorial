04df994a1fdeab747ef7d7d1721372ea548cedd3
Chris Wood <wchristopherwood912@gmail.com>
2022-09-03 18:32:32 -0700

    added comments to flash cards. removed ascii art

--- P01-Flash-Cards/flashcards.py
+++ P01-Flash-Cards/flashcards.py
@@ -3,34 +3,44 @@ import json
 score = 0
 numCorrect = 0
 
+#open and load json file
 with open("me-capitols.json", "r") as f:
     data = json.load(f)
-    print(data)
 
 cards = {}
 
+#initializes the data structure for the flash cards
 for i in data["cards"]:
     cards[i['q']] = i['a']
-print(cards.keys())
 
+#makes a list of keys for flashcards
 cardList = list(cards.keys())
 
+print("Welcome to flash cards! \nfor every question you get right you get points equal to your streak. your streak decreases by 1 if you get a question wrong. \nIf your streak is below zero you lose!")
+
+#play loop
 play = True
 while play:
+    #prints the questions
     for i in range(len(cardList)):
         print(f"{i}. {cardList[i]}")
     qnum = input("Enter the question number you would like to answer: ")
+    #checks for valid input
     if qnum.isnumeric():
         qnum = int(qnum)
+        #game logic
+        #TODO no longer accept answering the same question twice
         if input(f"{cardList[qnum]}: ").lower() == cards[cardList[qnum]].lower():
             numCorrect += 1
             score += numCorrect
             print(f"Correct! You earned {numCorrect}. your score is now {score}")
         else:
             numCorrect -=1
+            print(numCorrect)
             print(f"Incorrect, you have {2+numCorrect} more chances")
             if numCorrect < -1:
                 play = input("you lose! would you like to play again? y/n").lower()
+                numCorrect = 0
                 if play == "y":
                     play = True
                 elif play == "n":
--- P03-ASCII-Art/asciiart.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-text = input("enter the text you wish to turn into ascii")
-r = requests.get("https://ascii-generator.site/t/")
-print(r.text)
-print("done")
